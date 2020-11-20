4ec73b2e5d37a5f207005a52684edcb9481c0b73
Maxwell <thundermnt@gmail.com>
2020-11-20 14:19:56 +0300

    start

---
+++ Assets/scripts/ui/MovePadScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+
+public class MovePadScript : MonoBehaviour {
+
+    public RawImage MoveStick;
+    Vector3 pos;
+    bool hold = false;
+	// Use this for initialization
+	void Start () {
+        pos = MoveStick.rectTransform.position; // Начальная позиция стика
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(hold)MoveStick.rectTransform.position = Vector3.ClampMagnitude(Input.mousePosition - pos, 59.0f) + pos;
+    }
+    public void HoldDown()
+    {
+        hold = true;
+    }
+    public void HoldUp()
+    {
+        MoveStick.rectTransform.position = pos;
+        hold = false;
+    }
+
+}
